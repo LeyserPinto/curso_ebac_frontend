7034c97b15e71f12a87d1088e0a73004f525e52b
Leyser Pinto <59316466+LeyserPinto@users.noreply.github.com>
2023-05-19 22:35:40 -0300

    :tada: Exercicio TypeScript

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+const multiply = (num_a, num_b) => {
+    return num_a * num_b;
+};
+const sendGreet = (name) => {
+    return `Hi ${name}, is nice to meet you :)`;
+};
+console.log(multiply(2, 2));
+console.log(sendGreet('Leyser'));
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+
+
+const multiply = (num_a:number, num_b:number):number =>{
+    return num_a * num_b
+}
+
+const sendGreet = (name: string):string =>{
+    return `Hi ${name}, is nice to meet you :)`
+}
+
+
+console.log(multiply(2, 2))
+console.log(sendGreet('Leyser'))
